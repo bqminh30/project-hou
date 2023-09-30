637cb4ca1215382966eb481531ad7d60bdd7ca50
BuiQuangMinh30 <buiquangminh30102001@gmail.com>
2023-09-30 08:24:20 +0700

    project client

---
+++ client/src/routes/sections/index.tsx
@@ -0,0 +1,52 @@
+import { Navigate, useRoutes } from 'react-router-dom';
+// layouts
+// import MainLayout from 'src/layouts/main';
+import MainLayout from 'src/layouts/dashboard';
+// config
+// import { PATH_AFTER_LOGIN } from 'src/config-global';
+//
+import { mainRoutes, HomePage } from './main';
+import { authRoutes } from './auth';
+import { authDemoRoutes } from './auth-demo';
+import { dashboardRoutes, IndexPage } from './dashboard';
+import { componentsRoutes } from './components';
+
+// ----------------------------------------------------------------------
+
+export default function Router() {
+  return useRoutes([
+    // SET INDEX PAGE WITH SKIP HOME PAGE
+    // {
+    //   path: '/',
+    //   element: <Navigate to={PATH_AFTER_LOGIN} replace />,
+    // },
+
+    // ----------------------------------------------------------------------
+
+    // SET INDEX PAGE WITH HOME PAGE
+    {
+      path: '/',
+      element: (
+        <MainLayout>
+          <IndexPage />
+        </MainLayout>
+      ),
+    },
+
+    // Auth routes
+    // ...authRoutes,
+    // ...authDemoRoutes,
+
+    // Dashboard routes
+    ...dashboardRoutes,
+
+    // Main routes
+    // ...mainRoutes,
+
+    // Components routes
+    // ...componentsRoutes,
+
+    // No match 404
+    { path: '*', element: <Navigate to="/404" replace /> },
+  ]);
+}
